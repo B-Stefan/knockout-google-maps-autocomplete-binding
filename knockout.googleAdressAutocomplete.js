f3fbb4e43cbf2a4955b66d9da519a55d18fa8ce4
Stefan B. <nafets1129@gmail.com>
2013-05-08 13:54:39 +0200

    Add option to allow non place strings as value

--- knockout.googleAdressAutocomplete.js
+++ knockout.googleAdressAutocomplete.js
@@ -5,35 +5,40 @@ define(['knockout', 'jquery', 'gmaps'], function (ko, $, gmaps) {
             var self = element
             var allBindings = allBindingsAccessor()
             var position = new gmaps.maps.LatLng(allBindings.lat(), allBindings.lng());
+
             /**
              * Define geocode function for update and init
              */
             self.geocode = ko.computed({
                 write: function (pos) {
-                    if (self.useGeocoder == true) {
-                        if (allBindings.lat() == 0 || allBindings.lng() == 0) {
-                            return
-                        }
-                        if (allBindings.map) {
-                            viewport = allBindings.map
-                        }
-                        self._geocoder = new gmaps.maps.Geocoder();
-                        self._geocoder.geocode({'latLng': pos, region: 'de'}, function (results, status) {
-                            if (status == gmaps.maps.GeocoderStatus.OK) {
-                                if (results[0]) {
-                                    result = results[0]
-
-                                    $(self).val(result.formatted_address)
-                                    allBindings.value(result.formatted_address)
-                                } else {
-                                    alert("'No results found'")
-                                    window.dialog({state: false, message: 'No results found'});
-                                }
-                            } else {
-                                alert('Geocoder failed due to: ' + status);
-                            }
-                        });
+                    if (allBindings.lat() == 0 || allBindings.lng() == 0) {
+                        return
                     }
+                    if (allBindings.lat() == self._oldLat && allBindings.lng() == self._oldLng) {
+                        return
+                    }
+                    if (allBindings.map) {
+                        viewport = allBindings.map
+                    }
+                    self._geocoder = new gmaps.maps.Geocoder();
+                    self._geocoder.geocode({'latLng': pos, region: 'de'}, function (results, status) {
+                        if (status == gmaps.maps.GeocoderStatus.OK) {
+                            if (results[0]) {
+                                result = results[0]
+
+                                $(self).val(result.formatted_address)
+
+                                self._oldPlace = result.formatted_address
+                                allBindings.value(result.formatted_address)
+                            } else {
+                                alert("'No results found'")
+                                window.dialog({state: false, message: 'No results found'});
+                            }
+                        } else {
+                            alert('Geocoder failed due to: ' + status);
+                        }
+                    });
+
 
                 },
                 read: function () {
@@ -42,11 +47,14 @@ define(['knockout', 'jquery', 'gmaps'], function (ko, $, gmaps) {
             }).extend({throttle: 1000})
 
             if (allBindings.lat() != 0 && allBindings.lng() != 0 && $(self).val() == '') {
-                self.useGeocoder = true
                 self.geocode(position)
             }
 
-
+            /**
+             * Add autocomplete support for the input
+             * @type {gmaps.maps.places.Autocomplete}
+             * @private
+             */
             self._autocomplete = new gmaps.maps.places.Autocomplete(element);
             if (allBindings.map) {
                 self._autocomplete.bindTo('bounds', allBindings.map);
@@ -70,23 +78,40 @@ define(['knockout', 'jquery', 'gmaps'], function (ko, $, gmaps) {
                     }
                 }
                 //update model position
+                self._oldLat = place.geometry.location.lat()
+                self._oldLng = place.geometry.location.lng()
                 allBindings.lat(place.geometry.location.lat())
                 allBindings.lng(place.geometry.location.lng())
+
+                self._oldPlace = place.formatted_address
                 allBindings.value(place.formatted_address)
-
-
-                self.useGeocoder = false
-
 
             })
         },
         update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
             var self = element
-            var valueUnwrapped = ko.utils.unwrapObservable(allBindingsAccessor())
-            console.log("update")
+            if (valueAccessor().allowInvalidPlaceInput == true) {
+
+                currentValue = ko.utils.unwrapObservable(allBindingsAccessor()).value()
+                try{
+
+                    autocompleteValue = self._oldPlace
+
+                }
+                catch(err){
+                    return
+                }
+                console.log(currentValue + ' - ' +  autocompleteValue)
+                    if (currentValue != autocompleteValue ){
+                        console.log("yeeea")
+                        return
+                    }
+
+            }
+            valueUnwrapped = ko.utils.unwrapObservable(allBindingsAccessor())
             var position = new gmaps.maps.LatLng(valueUnwrapped.lat(), valueUnwrapped.lng());
             self.geocode(position)
-            self.useGeocoder = true
+
 
         }
 
